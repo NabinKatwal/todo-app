cb4a83ff51002442cc86c8c955c0a7308bc318f7
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-06-02 20:12:09 +0545

    db worksss!

---
+++ todo/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const app = express()
+const port = 4000 
+
+mongoose.connect('mongodb://localhost/todo', {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true,
+})
+
+const userSchema = new mongoose.Schema({
+    name:String, 
+    password: String,
+})
+
+const User = mongoose.model('User', userSchema)
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/register',async (req, res)=>{
+    const {username, password} = req.body
+    await User.create({ username, password })
+    res.json({
+        message: "success",
+    })
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+});
---
+++ todo/src/pages/Register.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+export default function Register() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const register = (e)=>{
+        e.preventDefault()
+        fetch(`http://localhost:4000/register`,{
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username, 
+                password,
+            })
+        })
+    }
+
+    return (
+        <div>
+            <h1>Register</h1>
+            <form onSubmit = {register}>
+                <input onChange = {(e)=> setUsername(e.target.value)} placeholder="username"/><br />
+                <input type="password" onChange = {(e)=> setPassword(e.target.value)} placeholder="password" /><br />
+                <button type="submit" >Register</button>
+            </form>
+        </div>
+    )
+}
